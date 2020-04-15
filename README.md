5862b0d7a9d253c627f17cc448302a10af04ffef
e <aluruihua@163.com>
2020-04-15 18:37:44 +0800

    acha

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # 定时任务的后台管理页面
 - [后台的java地址](https://github.com/Lu-dashuai/jack-email)
 - 前后台分离，等待茶姐加入，写前端。
+- 参考[elementUI官网](https://element.eleme.cn/#/zh-CN/component/installation)
+
+## 20200415
+![页面](./static/20200415.jpg)
+- 现在差 全部暂停按钮 全部开始按钮 编辑弹框 删除 添加弹框
+- 前期不要求分页,样式自己设计，没啥要求，好看点就行
+---
+
+1. 编辑弹框：
+- 只能修改jobName\  类名\  cron表达式的值。
+2. 删除：
+- 要求弹出确定删除的框，并且和操作分开写，以免误触。
+3. 添加：
+- 字段为：jobName\  CronExpression\  beanName
--- src/components/home.vue
+++ src/components/home.vue
@@ -3,23 +3,21 @@
     <el-table :data="tableData" style="width: 100%">
       <el-table-column prop="id" label="id" width="150">
       </el-table-column>
-      <el-table-column prop="job_name" label="jobName" width="150">
+      <el-table-column prop="jobName" label="jobName" width="150">
       </el-table-column>
-      <el-table-column prop="cron_expression" label="cron表达式" width="150">
+      <el-table-column prop="cronExpression" label="cron表达式" width="150">
       </el-table-column>
-      <el-table-column prop="bean_name" label="类名" width="150">
+      <el-table-column prop="beanName" label="类名" width="150">
       </el-table-column>
-      <el-table-column prop="method_name" label="默认执行方法" width="150">
+      <el-table-column prop="methodName" label="默认执行方法" width="150">
       </el-table-column>
       <el-table-column prop="status" label="当前状态" width="150">
       </el-table-column>
-      <el-table-column prop="delete_flag" label="是否删除" width="150">
+      <el-table-column prop="creatorName" label="创建者" width="150">
       </el-table-column>
-      <el-table-column prop="creator_name" label="创建者" width="150">
+      <el-table-column prop="createdTime" label="创建时间" width="150">
       </el-table-column>
-      <el-table-column prop="created_time" label="创建时间" width="150">
-      </el-table-column>
-      <el-table-column prop="updated_time" label="上次更新时间" width="150">
+      <el-table-column prop="updatedTime" label="上次更新时间" width="150">
       </el-table-column>
       <el-table-column fixed="right" label="操作" width="150">
         <template slot-scope="scope">
@@ -40,7 +38,7 @@
     },
     data() {
       return {
-        tableData: []
+        tableData: [{"id":9,"jobName":"任务01","cronExpression":"0/10 * * * * ?","beanName":"testJob01","methodName":"execute","status":1,"deleteFlag":false,"creatorId":1,"creatorName":"admin","createdTime":"2020-04-14T21:36:07","updatedTime":"2020-04-14T21:38:38"},{"id":10,"jobName":"任务02","cronExpression":"0/3 * * * * ?","beanName":"testJob02","methodName":"execute","status":1,"deleteFlag":false,"creatorId":1,"creatorName":"admin","createdTime":"2020-04-14T21:37:14","updatedTime":"2020-04-14T21:38:38"}]
       }
     }
   }
